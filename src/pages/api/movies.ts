3d8d6c29588ea66206ad54e925329f408453822a
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-09-18 10:49:43 -0500

    feat: Created api for movies fetch

---
+++ src/pages/api/movies.ts
@@ -0,0 +1,20 @@
+import { getMovies } from "@/lib/getMovies";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const query: any = req.query;
+
+    const newMovies = await getMovies({
+      ...query,
+      page: Number(query?.page!) + 1,
+    });
+
+    return res.status(200).json(newMovies);
+  } catch (e) {
+    return res.status(500).json(e);
+  }
+}
